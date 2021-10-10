05902ab6ea9e6e35bca06671a2dcbb8316643cfe
Dipanjan De <dipanjan131@gmail.com>
2021-10-10 18:07:34 +0530

    Injected i18n to store

--- src/main.js
+++ src/main.js
@@ -8,5 +8,6 @@ import store from "./store";
 import { createI18n } from "vue-i18n/index";
 import i18n_config from "@/plugins/i18n";
 const i18n = createI18n(i18n_config);
+store.$i18n = i18n; // Accessible via this.$i18n inside store
 
 createApp(App).use(store).use(router).use(i18n).mount("#app");
